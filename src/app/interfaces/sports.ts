ff08d6508f3eda7a347069f3e6c087099fb50a51
Kyle Vassella <kylevassella@gmail.com>
2022-02-20 09:15:29 -0600

    Completed component filters

---
+++ src/app/interfaces/sports.ts
@@ -0,0 +1,23 @@
+export interface ITennis {
+    looser: string;
+    numberOfSets: number;
+    publicationDate: string;
+    tournament: string;
+    winner: string;
+}
+
+export interface IF1 {
+    publicationDate: string;
+    seconds: number;
+    tournament: string;
+    winner: string;
+}
+
+export interface INba {
+    gameNumber: number;
+    looser: string;
+    mvp: string;
+    publicationDate: string;
+    tournament: string;
+    winner: string; 
+ }
